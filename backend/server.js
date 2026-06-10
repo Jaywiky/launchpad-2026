b87af945ed81826d6f4c53c0f916b119cf94c154
Jwiky2 <yrj24guu@uea.ac.uk>
2026-06-10 13:01:02 +0100

    clean up comments

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./src/middleware/errorHandler");
-// const resourceRoutes = require("./src/routes/resources");
 const manifestRoutes = require("./src/routes/manifest");
 
 const { bootSync, scheduledSync } = require("./src/services/syncService");
@@ -22,7 +21,6 @@ app.get("/health", (_req, res) => {
 });
 
 // Routes
-// app.use("/api", resourceRoutes);
 app.use("/api", manifestRoutes)
 
 // 404
